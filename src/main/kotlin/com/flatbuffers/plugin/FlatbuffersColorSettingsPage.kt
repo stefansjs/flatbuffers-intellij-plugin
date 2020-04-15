d4c0292c1aec2158f990a1d458f459bb34adb3ae
Stefan Sullivan <stefan.sullivan@gmail.com>
2020-04-15 16:46:13 -0700

    Flatbuffers color settings page

---
+++ src/main/kotlin/com/flatbuffers/plugin/FlatbuffersColorSettingsPage.kt
@@ -0,0 +1,57 @@
+package com.flatbuffers.plugin
+
+import com.flatbuffers.plugin.icons.FlatbuffersIcon
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.options.colors.AttributesDescriptor
+import com.intellij.openapi.options.colors.ColorDescriptor
+import com.intellij.openapi.options.colors.ColorSettingsPage
+
+
+class FlatbuffersColorSettingsPage: ColorSettingsPage {
+    companion object {
+        val DESCRIPTORS = arrayOf(
+            AttributesDescriptor("Comment", FlatbuffersSyntaxHighlighter.COMMENT),
+            AttributesDescriptor("Declaration", FlatbuffersSyntaxHighlighter.CLASS_REFERENCE),
+            AttributesDescriptor("Object", FlatbuffersSyntaxHighlighter.CLASS_REFERENCE)
+        )
+    }
+
+    override fun getIcon() = FlatbuffersIcon.ICON
+    override fun getHighlighter() = FlatbuffersSyntaxHighlighter()
+    override fun getDisplayName() = FlatbuffersLanguage.displayName
+
+    override fun getAttributeDescriptors() = DESCRIPTORS
+    override fun getColorDescriptors(): Array<ColorDescriptor> = ColorDescriptor.EMPTY_ARRAY!!
+    override fun getAdditionalHighlightingTagToDescriptorMap(): MutableMap<String, TextAttributesKey>? = null
+
+    override fun getDemoText() = """
+        |// example IDL file
+        |
+        |namespace MyGame;
+        |
+        |attribute "priority";
+        |
+        |enum Color : byte { Red = 1, Green, Blue }
+        |
+        |union Any { Monster, Weapon, Pickup }
+        |
+        |struct Vec3 {
+        |  x:float;
+        |  y:float;
+        |  z:float;
+        |}
+        |
+        |table Monster {
+        |  pos:Vec3;
+        |  mana:short = 150;
+        |  hp:short = 100;
+        |  name:string;
+        |  friendly:bool = false (deprecated, priority: 1);
+        |  inventory:[ubyte];
+        |  color:Color = Blue;
+        |  test:Any;
+        |}
+        |
+        |root_type Monster;
+        |""".trimMargin()
+}
