83ddb1d37b31c75a6e7068513c9e8dc29544aec6
Stefan Sullivan <stefan.sullivan@gmail.com>
2020-05-25 22:59:03 -0700

    Simply the type reference implementation: I don't need to modify the text range for the reference

--- src/main/kotlin/io/github/stefansjs/flatbuffersplugin/psi/ref/FlatbuffersTypeReference.kt
+++ src/main/kotlin/io/github/stefansjs/flatbuffersplugin/psi/ref/FlatbuffersTypeReference.kt
@@ -2,7 +2,6 @@ package io.github.stefansjs.flatbuffersplugin.psi.ref
 
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementResolveResult
 import com.intellij.psi.PsiReferenceBase
@@ -10,10 +9,8 @@ import com.intellij.psi.ResolveResult
 import io.github.stefansjs.flatbuffersplugin.icons.FlatbuffersIcon.FILE
 import io.github.stefansjs.flatbuffersplugin.psi.impl.findTypes
 
-class FlatbuffersTypeReference(element: PsiElement, textRange: TextRange): PsiReferenceBase<PsiElement>(element, textRange)
+class FlatbuffersTypeReference(element: PsiElement): PsiReferenceBase<PsiElement>(element)
 {
-    private val typeName = element.text.substring(textRange.startOffset, textRange.endOffset)
-
     override fun resolve(): PsiElement? {
         // Try and find the declaration locally first. IIUC flatbuffers only allows one declaratino per namespace,
         // which means finding more than one result is actually an error
@@ -31,12 +28,12 @@ class FlatbuffersTypeReference(element: PsiElement, textRange: TextRange): PsiRe
     }
 
     private fun multiResolve(): Array<ResolveResult> {
-        val declarations = findTypes(myElement.project, typeName)
+        val declarations = findTypes(element.project, element.text)
         return declarations.map { PsiElementResolveResult(it) }.toTypedArray()
     }
 
     override fun getVariants(): Array<LookupElement> {
-        val declarations = findTypes(myElement.project)
+        val declarations = findTypes(element.project)
         val variants = declarations.filter { it.name != null && it.name!!.isNotEmpty() }
         return variants.map {
             LookupElementBuilder.create(it).withIcon(FILE).withTypeText(it.containingFile.name)
