23ce7beced6ed1d8aac9f171c18e0947724e85cb
Stefan Sullivan <stefan.sullivan@gmail.com>
2020-06-20 14:28:24 -0700

    Fix qualified type reference bug
    Type references were searching using the whole string. They
    needed to use just the unqualified component because type
    declarations don't actually contain the entire namespace
    dotted name.
    
    This still does not actually use the namespace when
    searching for types, which means the same name used in
    multiple namespaces will not resolve at all.

--- src/main/kotlin/io/github/stefansjs/flatbuffersplugin/psi/ref/FlatbuffersTypeReference.kt
+++ src/main/kotlin/io/github/stefansjs/flatbuffersplugin/psi/ref/FlatbuffersTypeReference.kt
@@ -14,22 +14,27 @@ import com.intellij.psi.search.FileTypeIndex
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.util.PsiTreeUtil
 import io.github.stefansjs.flatbuffersplugin.FlatbuffersFileType
+import io.github.stefansjs.flatbuffersplugin.psi.FlatbuffersDeclaredType
 import io.github.stefansjs.flatbuffersplugin.psi.FlatbuffersEnumDecl
 import io.github.stefansjs.flatbuffersplugin.psi.FlatbuffersTypeDecl
 import io.github.stefansjs.flatbuffersplugin.psi.FlatbuffersUnionDecl
 
-class FlatbuffersTypeReference(element: PsiElement):
-    PsiReferenceBase<PsiElement>(element, TextRange(0, element.textLength))
+class FlatbuffersTypeReference(element: FlatbuffersDeclaredType) :
+    PsiReferenceBase<FlatbuffersDeclaredType>(element,
+                                              TextRange(element.ident.startOffsetInParent,
+                                                        element.ident.startOffsetInParent + element.ident.textLength))
 {
     override fun resolve(): PsiElement? {
         // Try and find the declaration locally first. IIUC flatbuffers only allows one declaratino per namespace,
         // which means finding more than one result is actually an error
-        val localDeclarations = findTypes(element.containingFile, element.text)
+        val typeText = rangeInElement.substring(element.text)
+
+        val localDeclarations = findTypes(element.containingFile, typeText)
         if (localDeclarations.size == 1) {
             return localDeclarations[0]
         }
 
-        val resolveResult = findTypes(element.project, element.text)
+        val resolveResult = findTypes(element.project, typeText)
         if (resolveResult.size == 1){
             return resolveResult[0]
         }
@@ -53,7 +58,7 @@ class FlatbuffersTypeReference(element: PsiElement):
 }
 
 // Reference resolvers. Based (conveniently) on the above getName implementations
-private fun findTypes(project: Project, typeName: String?=null): List<FlatbuffersNamedElement>
+private fun findTypes(project: Project, typeName: String? = null): List<FlatbuffersNamedElement>
 {
     // Try the current file first
     val results = ArrayList<FlatbuffersNamedElement>()
